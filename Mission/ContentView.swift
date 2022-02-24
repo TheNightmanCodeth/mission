e799194f060e549b2666a6376da429378f69bf50
Joe Diragi <joyod3@gmail.com>
2022-02-24 16:13:36 -0500

    Initial Commit

---
+++ Mission/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mission
+//
+//  Created by Joe Diragi on 2/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mission/MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissionApp.swift
+//  Mission
+//
+//  Created by Joe Diragi on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
