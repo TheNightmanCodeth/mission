13e13b293e6ac0259b502ee6e48ba6ed075febc7
Joe Diragi <joyod3@gmail.com>
2022-08-31 02:54:13 -0400

    Adds code sign identity and provisioning profile uuid

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -7,4 +7,6 @@ xcodebuild -workspace Mission.xcworkspace \
 	-configuration Release \
 	-allowProvisioningUpdates \
 	-archivePath $PWD/build/Mission.xcarchive \
+	CODE_SIGN_IDENTITY="3rd Party Mac Developer Application: Joe Diragi (D44Y5BBJ48)" \
+	PROVISIONING_PROFILE="5c49ec53-011d-4269-b2d6-bfa49d5e42f6" \
 	clean archive
